dc0448fd4f30532dc5bd992d50964b7d5b2ce411
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-26 17:49:53 +0600

    react flder structure setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    <Toaster position="top-right" reverseOrder={false} />
+  </React.StrictMode>
+);
