edae01c8ed787b5134550a8505c78b7e1ea18541
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-26 18:23:47 +0600

    auth ocntext, custom hoook useAuth andprivate route implemented done

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,13 +1,64 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/firebase.config";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut= () => {
+        setLoading(true);
+        return signOut(auth);  
+    }
+
+    const signInWIthMail = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const userUpdateProfile = (name, photo) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+          })
+        }
+    
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+
+            setLoading(false);
+        });
+
+        return () => {
+            return unsubscribe();
+        }
+    }, []);
 
     const authInfo = {
-
+        user, 
+        loading,
+        createUser,
+        signInWIthMail,
+        logOut,
+        userUpdateProfile,
+        signInWithGoogle
     }
 
     return (
