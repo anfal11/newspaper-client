7870d62616016d2398be62c3615d98cd07ed75eb
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-27 06:44:32 +0600

    all articles added and tanstack query installed

--- src/Pages/Articles/AllArticles.jsx
+++ src/Pages/Articles/AllArticles.jsx
@@ -1,8 +1,15 @@
+import axios from "axios";
 
 const AllArticles = () => {
+
+    axios.get('http://localhost:5000/articles')
+    .then(res => {
+        console.log(res.data);
+    })
+
     return (
         <div>
-            All articles
+            <h1 className="text-3xl underline font-bold text-center pt-28 text-gray-500"> All Articles </h1>
         </div>
     );
 };
