36c184197bb53d5119e56db010fb69123a6e3268
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-27 08:00:02 +0600

    All article implemented and article card implemented with materialui

--- src/Component/ArticleCard/ArticleCard.jsx
+++ src/Component/ArticleCard/ArticleCard.jsx
@@ -1,12 +1,84 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import useAuth from "../../Hooks/useAuth";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const ArticleCard = ({article}) => {
-    const { _id, title, image, publisher, tags, "short-description": shortDescription, "long-description": longDescription} = article;
-    return (
-        <div>
-                {_id}
-                {title}
-        </div>
-    );
+const ArticleCard = ({ article }) => {
+  const { user } = useAuth();
+
+  const {
+    _id,
+    title,
+    image,
+    publisher,
+    tags,
+    "short-description": shortDescription,
+    "long-description": longDescription,
+  } = article;
+
+  const { refetch, data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/users");
+      return res.data;
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  const matchedUser = users?.find((user) => user?.name === publisher);
+  const userImage = matchedUser ? matchedUser.image : null;
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div></div>
+      <div>
+        <Card sx={{ maxWidth: 345, height: 550}}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            image={image}
+            className="h-60 w-full object-cover"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {shortDescription}
+            </Typography>
+          </CardContent>
+          <CardActions className="flex items-center gap-2">
+            <img className="rounded-full h-8 w-8" src={userImage} alt="" />
+            <Button size="small">{publisher}</Button>
+          </CardActions>
+          <CardActions>
+            <Button size="small">
+              {tags.map((tag) => (
+                <span className="bg-gray-200 rounded-full px-1 py-2 text-sm font-semibold text-gray-700 mr-2">
+                  {tag}
+                </span>
+              ))}
+            </Button>
+          </CardActions>
+            <Link>
+            <Button size="small">Details</Button>
+            </Link>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleCard;
--- src/Pages/Articles/AllArticles.jsx
+++ src/Pages/Articles/AllArticles.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import ArticleCard from "../../Component/ArticleCard/ArticleCard";
+import useAuth from "../../Hooks/useAuth";
 
 const AllArticles = () => {
-
-    const { refetch, data: articles = []} = useQuery({
+    const { loading: authLoading} = useAuth();
+    const { refetch, data: articles = [], isLoading: usersLoading} = useQuery({
         queryKey: ["articles"],
         queryFn: async () => {
             const res =  await axios.get("http://localhost:5000/articles");
@@ -13,12 +14,26 @@ const AllArticles = () => {
     });
     refetch();
 
+    const loading = authLoading || usersLoading;
+
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
             <h1 className="text-3xl underline font-bold text-center pt-28 text-gray-500"> All Articles </h1>
+            {/* todo: add search and filter field */}
+            <h1 className="my-10"> Search here</h1>
+            <h1 className="my-10"> Filter By</h1>
+            {
+                loading ? <h1>Loading...</h1> 
+                : 
+                <>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 lg:p-0 gap-4">
 
             {
                 articles.map(article => <ArticleCard key={article._id} article={article} />)
+            }
+            </div>
+                </>
             }
         </div>
     );
